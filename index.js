0fcd8f9bbb8ed38430c04ad1d1af3aecd81b739e
Debian <santo@santovm.mtu1vz3vv2pe3nczwgcpxskfdd.fx.internal.cloudapp.net>
2024-03-04 12:23:59 +0000

    certificate + echo server

--- index.js
+++ index.js
@@ -1,26 +1,49 @@
-const http = require("http");
+const fs = require('fs');
+const https = require("https");
 const express = require("express");
 const socketIo = require("socket.io");
 
+//Certificate
+const privateKey = fs.readFileSync('/etc/letsencrypt/live/santo-chat.northeurope.cloudapp.azure.com/privkey.pem', 'utf8');
+const certificate = fs.readFileSync('/etc/letsencrypt/live/santo-chat.northeurope.cloudapp.azure.com/cert.pem', 'utf8');
+const ca = fs.readFileSync('/etc/letsencrypt/live/santo-chat.northeurope.cloudapp.azure.com/chain.pem', 'utf8');
+const credentials = {
+  key: privateKey,
+  cert: certificate,
+  ca: ca
+
+};
+
 const app = express();
-const server = http.createServer(app);
+const server = https.createServer(credentials, app);
 const io = socketIo(server);
 
-app.use(express.static(__dirname + "public"));
+app.use(express.static(__dirname + "/public",  { dotfiles: 'allow'  }));
 
-io.on("connection", (socket) => {
-    console.log("user connected");
+let numUsers = 0;
 
-    socket.on("disconnect", () => {
-        console.log("user disconnected");
-    });
+io.on('connection', (socket) => {
+  let addedUser = false;
+  console.log("User connected")
 
-    socket.on("chat_message", (msg) => {
-        io.emit("chat message", msg);
-    });
+  socket.on('chat message', (data) => {
+    console.log("Got chat message")
+    // we tell the client to execute 'chat message'
+    io.emit('chat message', data);
+  });
+     socket.on('disconnect', () => {
+         if (addedUser) {
+  
+           // echo globally that this client has left
+           socket.broadcast.emit('user left', {
+             username: socket.username,
+             numUsers: numUsers
+           });
+         }
+     });
 });
 
-const port = process.env.PORT || 80;
+const port = process.env.PORT || 443;
 
 server.listen(port, () => {
     console.log("server running on port " + port);
