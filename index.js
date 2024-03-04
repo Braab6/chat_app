501a0c11a249f90ab592db2b90a152b2836212bd
Braab6 <67276568+Braab6@users.noreply.github.com>
2024-03-04 10:47:35 +0100

    first version

---
+++ index.js
@@ -0,0 +1,41 @@
+const http = require("http");
+const express = require("express");
+const socketio = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+const port = process.env.PORT || 3000;
+
+server.listen(port, function () {
+    console.log("webserver running on port %d", port);
+});
+
+app.use(express.static(__dirname + "/public"));
+
+io.on("connection", function (socket) {
+    var addedUser = false;
+
+    socket.on("add user", function (username) {
+        socket.username = username;
+        addedUser = true;
+
+        socket.emit("login");
+
+        socket.broadcast.emit("user joined", socket.username);
+    });
+
+    socket.on("new message", function (data) {
+        socket.broadcast.emit("new message", {
+            username: socket.username,
+            message: data
+        });
+    });
+
+    socket.on("disconnect", function () {
+        if (addedUser) {
+            socket.broadcast.emit("user left", socket.username);
+        }
+    });
+});
