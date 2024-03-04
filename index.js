8d98dffa52f7faa003129b1ff6ca6f7604bbd697
temporary72 <em4cally2git@online.de>
2024-03-04 16:33:26 +0100

    stuff

--- index.js
+++ index.js
@@ -1,47 +1,54 @@
-const fs = require('fs');
+const fs = require("fs");
 const https = require("https");
 const express = require("express");
 const socketIo = require("socket.io");
 
-//Certificate
-const privateKey = fs.readFileSync('/etc/letsencrypt/live/santo-chat.northeurope.cloudapp.azure.com/privkey.pem', 'utf8');
-const certificate = fs.readFileSync('/etc/letsencrypt/live/santo-chat.northeurope.cloudapp.azure.com/cert.pem', 'utf8');
-const ca = fs.readFileSync('/etc/letsencrypt/live/santo-chat.northeurope.cloudapp.azure.com/chain.pem', 'utf8');
-const credentials = {
-  key: privateKey,
-  cert: certificate,
-  ca: ca
+const privateKey = fs.readFileSync("/etc/letsencrypt/live/santo-chat.northeurope.cloudapp.azure.com/privkey.pem", "utf8");
+const certificate = fs.readFileSync("/etc/letsencrypt/live/santo-chat.northeurope.cloudapp.azure.com/cert.pem", "utf8");
+const ca = fs.readFileSync("/etc/letsencrypt/live/santo-chat.northeurope.cloudapp.azure.com/chain.pem", "utf8");
 
+const credentials = {
+    "key": privateKey,
+    "cert": certificate,
+    "ca": ca
 };
 
 const app = express();
 const server = https.createServer(credentials, app);
 const io = socketIo(server);
 
-app.use(express.static(__dirname + "/public",  { dotfiles: 'allow'  }));
+app.use(express.static(__dirname + "/public",  { dotfiles: "allow" }));
 
 let numUsers = 0;
+let history = {};
 
-io.on('connection', (socket) => {
-  let addedUser = false;
-  console.log("User connected")
+io.on("connection", (socket) => {
+    let addedUser = false;
+    console.log("user connected");
 
-  socket.on('chat message', (data) => {
-    console.log("Got chat message")
-    // we tell the client to execute 'chat message'
-    io.emit('chat message', data);
-  });
-     socket.on('disconnect', () => {
-         if (addedUser) {
-  
-           // echo globally that this client has left
-           socket.broadcast.emit('user left', {
-             username: socket.username,
-             numUsers: numUsers
-           });
-         }
-     });
+    socket.on("username", (username) => {
+        console.log("User " + username + "connected");
+        socket.username = username;
+        addedUser = true;
+    });
+    
+    socket.on("chat_message", (data) => {
+        console.log("received chat message");
+        const message = { "name": socket.username, "messageContent": data };
+        history[Date.now()].push(message);
+        io.emit("chat_message", message);
+    });
+
+    socket.on("disconnect", () => {
+        if (addedUser) {
+            socket.broadcast.emit("user left", {
+                username: socket.username,
+                numUsers: numUsers
+            });
+        }
+    });
 });
+
 
 const port = process.env.PORT || 443;
 
---
+++ public/script.js
@@ -0,0 +1,29 @@
+document.addEventListener("DOMContentLoaded", function () {
+    console.log("Initiating app")
+    const socket = io();
+
+    const text_input = document.getElementById("text_input");
+    const send_button = document.getElementById("send_button");
+    const chat_content = document.getElementById("chat_content");
+
+    send_button.onclick = function(event) {
+        if (text_input.value) {
+            console.log("sending_message")
+            socket.emit("chat_message", text_input.value);
+            text_input.value = "";
+        }
+    };
+
+    socket.on("connect_error", (err) => {
+        console.log(err.message);
+        console.log(err.description);
+        console.log(err.context);
+    });
+
+    socket.on("chat_message", (msg) => {
+        console.log("[INFO] message:" + msg);
+        const item = document.createElement("li");
+        item.textContent = msg;
+        chat_content.appendChild(item);
+    });
+});
