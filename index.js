7bde6162e8a678d12bf1a6ac969e603c5db25b11
Braab6 <67276568+Braab6@users.noreply.github.com>
2024-03-04 11:56:33 +0100

    cdn.socket

--- index.js
+++ index.js
@@ -23,5 +23,5 @@ io.on('connection', (socket) => {
 });
 
 server.listen(80, () => {
-  console.log('Server is running on http://localhost:3000');
+  console.log('Server is running on http://localhost:80');
 });
