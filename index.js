66a1eef87589f328984da89b718db992bb9af731
unknown <johannes.modler@gmx.net>
2024-03-06 15:18:41 +0100

    register 8 debug

--- index.js
+++ index.js
@@ -43,6 +43,8 @@ io.on("connection", (socket) => {
 
     socket.on("register", (credentials) => {
         console.log(credentials)
+        console.log(Object.keys(accounts))
+        console.log(accounts[credentials["name"]])
         if (Object.keys(accounts).includes(accounts[credentials["name"]])) {
             console.log("user does already exist")
         } else {
