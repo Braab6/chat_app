bea99332881db97d604c271db8c8055ab948459c
unknown <johannes.modler@gmx.net>
2024-03-06 11:40:47 +0100

    fix

--- public/login/login.js
+++ public/login/login.js
@@ -11,7 +11,10 @@ document.addEventListener("DOMContentLoaded", function () {
         console.log(username.value);
         console.log(password.value);
         socket.emit("login", { "name": username.value, "password": password.value });
-
         window.location.href = "https://santo-chat.northeurope.cloudapp.azure.com/chat/chat.html";
     }
+
+    socket.on("user_joined", (name) => {
+        console.log("suhuhewg" + name);
+    });
 });
