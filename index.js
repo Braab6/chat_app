5aece00f33d4f1849ac32c33ea860292cad53d9e
unknown <johannes.modler@gmx.net>
2024-03-07 10:14:03 +0100

    debug

--- index.js
+++ index.js
@@ -179,6 +179,10 @@ io.on("connection", (socket) => {
         
         keys = keys.reverse();
 
+        console.log(messages)
+        console.log(output)
+        console.log(number)
+        console.log(keys)
         for (i = 0; i < number; i++) {
             output[keys[i]].push[messages[keys[i]]];
         }
