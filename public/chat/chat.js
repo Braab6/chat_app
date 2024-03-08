afa523d027b88761cfb37251fb364f6fb4fb39f8
unknown <johannes.modler@gmx.net>
2024-03-08 10:34:45 +0100

    mir egal

--- public/chat/chat.js
+++ public/chat/chat.js
@@ -140,7 +140,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
     setInterval(() => {
         if (chat_area.scrollTop <= 0) {
-            const time_last_message = messages.length === 0 ? 0 : messages[0];
+            const time_last_message = messages.length === 0 ? 0 : Object.keys(messages)[0];
             console.log("time_last_message " + time_last_message);
             console.log(messages);
             socket.emit("request_recent", { "conversation" : conversation, "amount" : 3, "time" : time_last_message });
